8ab2d021ea613bb795a2d6a4ad5598b9a59506bc
elisar4 <elisar4@yandex-team.com>
2024-09-03 10:46:34 +0300

    add `ImageEffect` processing capability for `RemoteImageView` 40682fb5e3525306267e2f3b5dc32a63becd14e8

--- VGSLUI/RemoteImageView.swift
+++ VGSLUI/RemoteImageView.swift
@@ -13,6 +13,17 @@ public final class RemoteImageView: UIView, RemoteImageViewContentProtocol {
     return mask
   }()
 
+  private var visualEffectView: UIVisualEffectView? {
+    didSet {
+      if let visualEffectView, visualEffectView.superview == nil {
+        addSubview(visualEffectView)
+      }
+      if visualEffectView !== oldValue {
+        oldValue?.removeFromSuperview()
+      }
+    }
+  }
+
   public var appearanceAnimation: ImageViewAnimation?
 
   private var templateLayer: CALayer?
@@ -20,7 +31,8 @@ public final class RemoteImageView: UIView, RemoteImageViewContentProtocol {
   private func updateContent() {
     let content = Content(
       image: image,
-      imageRedrawingColor: imageRedrawingStyle?.tintColor
+      imageRedrawingColor: imageRedrawingStyle?.tintColor,
+      effects: imageRedrawingStyle?.effects ?? []
     )
 
     let gravity = imageContentMode
@@ -43,6 +55,28 @@ public final class RemoteImageView: UIView, RemoteImageViewContentProtocol {
         template.contentsGravity = gravity
         contentsLayer.mask = template
         templateLayer = template
+      case let .effects(image, effects):
+        var finalImage = image
+        for effect in effects {
+          switch effect {
+          case let .tint(color, mode):
+            if let out = finalImage.withColorBlend(color.systemColor, mode: mode ?? .sourceIn) {
+              finalImage = out
+            }
+          case .blur:
+            let effectView = visualEffectView ?? UIVisualEffectView()
+            effectView.effect = UIBlurEffect(style: .light)
+            visualEffectView = effectView
+          }
+        }
+        contentsLayer.setContents(finalImage)
+        contentsLayer.contentsGravity = gravity
+        contentsLayer.backgroundColor = nil
+        contentsLayer.mask = nil
+        templateLayer = nil
+      }
+      if !content.needsVisualEffectView {
+        visualEffectView = nil
       }
     }
     setNeedsLayout()
@@ -106,6 +140,8 @@ public final class RemoteImageView: UIView, RemoteImageViewContentProtocol {
     templateLayer?.contentsRect = layout.contentRect
     templateLayer?.contentsCenter = layout.contentCenter
 
+    visualEffectView?.frame = bounds
+
     updateMask()
   }
 
@@ -165,9 +201,27 @@ extension CALayer {
 private enum Content {
   case plain(Image?)
   case template(Image, color: Color)
+  case effects(Image, effects: [ImageEffect])
 
-  init(image: Image?, imageRedrawingColor: Color?) {
-    if let image, let color = imageRedrawingColor {
+  var needsVisualEffectView: Bool {
+    switch self {
+    case .effects(_, let effects):
+      for effect in effects {
+        switch effect {
+        case .blur: return true
+        default: continue
+        }
+      }
+      return false
+    default:
+      return false
+    }
+  }
+
+  init(image: Image?, imageRedrawingColor: Color?, effects: [ImageEffect]) {
+    if let image, effects.count > 0 {
+      self = .effects(image, effects: effects)
+    } else if let image, let color = imageRedrawingColor {
       self = .template(image, color: color)
     } else {
       self = .plain(image)
@@ -180,6 +234,41 @@ extension URLRequestResult.Source {
     switch self {
     case .network: true
     case .cache: false
+    }
+  }
+}
+
+private extension UIImage {
+  ///
+  /// be careful with scale - it can n^2 memory usage
+  ///
+  func withColorBlend(_ color: UIColor, mode: TintMode, scale: CGFloat = 1.0) -> UIImage? {
+    let drawRect = CGRect(origin: .zero, size: size)
+    UIGraphicsBeginImageContextWithOptions(drawRect.size, true, scale)
+    color.setFill()
+    UIRectFill(drawRect)
+    draw(in: drawRect, blendMode: mode.cgBlendMode, alpha: 1.0)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image
+  }
+}
+
+private extension TintMode {
+  var cgBlendMode: CGBlendMode {
+    switch self {
+    case .sourceIn:
+        .sourceIn
+    case .sourceAtop:
+        .sourceAtop
+    case .darken:
+        .darken
+    case .lighten:
+        .lighten
+    case .multiply:
+        .multiply
+    case .screen:
+        .screen
     }
   }
 }
