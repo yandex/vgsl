699a8e11ff8cc6c7eb95a7860ca9a2ac798564bf
booster <booster@yandex-team.com>
2025-08-29 15:36:16 +0300

    Removed height checking assert commit_hash:8742b4e3c060595474d013853ca5515df3bad176

--- VGSLUI/extensions/StringExtensions.swift
+++ VGSLUI/extensions/StringExtensions.swift
@@ -3,14 +3,10 @@ import Foundation
 
 extension String {
   public func with(typo: Typo) -> NSAttributedString {
-    assert(typo.isHeightSufficient)
-
     return NSAttributedString(string: self, attributes: typo.attributes)
   }
 
   public func mutable(withTypo typo: Typo) -> NSMutableAttributedString {
-    assert(typo.isHeightSufficient)
-
     return NSMutableAttributedString(string: self, attributes: typo.attributes)
   }
 }
